00438bc1eb95df27d310b336911e66d677f456ec
n.melnikov <n.melnikov@depra.org>
2025-02-05 01:47:51 +0400

    [v0.1.2] Add RagdollBehaviours

---
+++ Runtime/Plugins/RagdollBehaviours.cs
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+// © 2024-2025 Nikolay Melnikov <n.melnikov@depra.org>
+
+using Depra.Ragdoll.Plugins;
+using UnityEngine;
+using static Depra.Ragdoll.Module;
+
+namespace DefaultNamespace
+{
+	[AddComponentMenu(MENU_PATH + nameof(RagdollBehaviours), DEFAULT_ORDER)]
+	internal sealed class RagdollBehaviours : ExternalRagdollPlugin
+	{
+		[SerializeField] private Behaviour[] _behaviours;
+
+		protected override void OnEnabled()
+		{
+			foreach (var ik in _behaviours)
+			{
+				ik.enabled = false;
+			}
+		}
+
+		protected override void OnDisabled()
+		{
+			foreach (var ik in _behaviours)
+			{
+				ik.enabled = true;
+			}
+		}
+	}
+}
